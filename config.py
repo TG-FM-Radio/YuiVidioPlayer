d65775b0173af7432108fef053e2b5b9cc6ee8e0
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-09-09 19:51:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "6"))
-API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-SESSION_NAME = os.getenv("SESSION_NAME")
-ADMIN = os.getenv("ADMIN")
-DB_URL = os.getenv("DB_URL")
-DB_NAME = os.getenv("DB_NAME")
+API_ID = int(os.getenv("API_ID", "2107384"))
+API_HASH = os.getenv("API_HASH", "43ed88f662a3fcdaf787fef864fb37fa")
+BOT_TOKEN = os.getenv("1929345356:AAEY0BZOVB58QS8iJVWqB5OUc3v2aU5XX2I")
+SESSION_NAME = os.getenv("BQAao-CYD6tpJOSe1TXz6Xilmzu4HxOKvKY4ZaWQ8SSEu88cjfxReJ2elmiF2k9ZHdO-88fN3ijkylLJMJjtI-tbtkFtomuYEacsrFaAI9lPT_eg6PoNBbLbuKRop6G1105v1cRI28uww68ZlkuRjLeavb6WHVtrCKItFrv3DFwtHAP8tJC7ZvycB53Ds9c80NKN8N8v3RR_GRtH231DD1PpI5v2Juc9sCJBTVTkP1u4aKy27AyIkuye0lj4XiWgh4_iZqo0TJu2RZfDbyzO4nyVIFWrPOMTo-S0Wr6GLD1RfWGr5-nEPk84klPeXT3W_qyf-PqyZ2KpUQLFksSRO0LhUtHJBAA")
+ADMIN = os.getenv("1221558981")
+DB_URL = os.getenv("mongodb+srv://s1com:s1com@cluster0.wrrwz.mongodb.net/Cluster0?retryWrites=true&w=majority")
+DB_NAME = os.getenv("my")
